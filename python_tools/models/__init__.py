06e0f184a2c40d52feb06c38ae5b1ec508a2953f
Yuki Nishiwaki <uckey.1067@gmail.com>
2016-12-11 16:50:54 +0000

    Support connection of volume

--- python_tools/models/__init__.py
+++ python_tools/models/__init__.py
@@ -8,22 +8,25 @@ from collections import defaultdict
 import yaml
 
 
-def get_volume_containers(component_info):
+def get_volume_containers(component_info, component):
     result = []
     if 'volumes_from' not in component_info:
         return set(result)
     for v in component_info['volumes_from']:
         if not re.search(r':', v):
-            result.append(v)
+            result.append((component, v))
     return set(result)
 
 # Move volume container from normal group to special group
 # which is named as Volumes
+# <team>: [(sv, volume_container_name)]
 def move_volumes_from_sg(team_volumes_map):
     i = 0;
     for team, c_name_set in team_volumes_map.iteritems():
         i += 1
-        for c_name in c_name_set:
+        for sv_c_set in c_name_set:
+            sv_from = sv_c_set[0]
+            c_name = sv_c_set[1]
             sv = Service.find_service_by_name(c_name)
             if sv is None:
                 print "not found service: %s" % c_name
@@ -32,6 +35,12 @@ def move_volumes_from_sg(team_volumes_map):
             team.add_service(component_group_obj)
             sv.parent.remove_child_by_key(sv.key)
             component_group_obj.add_chidren(sv)
+
+            dep = Dependency(sv_from, sv)
+            dep.set_instance_of_from(sv_from)
+            dep.set_instance_of_to(sv)
+            sv_from.dependencies.append(dep)
+            sv.reffered_from.append(dep)
 
 def load_teams_and_services(file_name, shared=False):
     """ Initialize Team.TEAM_LIST
@@ -53,6 +62,6 @@ def load_teams_and_services(file_name, shared=False):
         team_obj.add_service(component_group_obj)
         component = Service.create_service(component_name, description, dependencies)
         component_group_obj.add_chidren(component)
-        team_volumes_map[team_obj] |= get_volume_containers(info)
+        team_volumes_map[team_obj] |= get_volume_containers(info, component)
 
     move_volumes_from_sg(team_volumes_map)
