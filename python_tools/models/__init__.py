ea0c70bc33489ad0cebecc4c8698fdecd0c1a52e
Yuki Nishiwaki <uckey.1067@gmail.com>
2016-12-11 15:08:41 +0000

    Refactoring
    - Team.key is created when init is executed
    - Disable Volume container
    - Gather logic about outputting graph data into one method
    - enable default value

--- python_tools/models/__init__.py
+++ python_tools/models/__init__.py
@@ -17,19 +17,14 @@ def load_teams_and_services(file_name, shared=False):
     docker_compose_data = yaml.load(docker_compose_fd)
 
     for component_name, info in docker_compose_data['services'].iteritems():
-        team = get_something_from_label(info, 'team')
+        team = get_something_from_label(info, 'team', 'unassign')
         description = get_something_from_label(info, 'description')
         component_group = get_something_from_label(info, 'component')
         dependencies_list = get_something_from_label(info, 'depends')
+
         dependencies = Dependency.parse_depenencies(component_name, dependencies_list)
-
         team_obj = Team.create_team(team, shared_service_maintainer=shared)
-
-        if is_volume_container(info, shared):
-            component_group_obj = ServiceGroup.create("Volumes")
-        else:
-            component_group_obj = ServiceGroup.create(component_group)
-
+        component_group_obj = ServiceGroup.create(component_group)
         team_obj.add_service(component_group_obj)
         component = Service.create_service(component_name, description, dependencies)
         component_group_obj.add_chidren(component)
